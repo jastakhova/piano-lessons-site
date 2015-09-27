5108c13b23e0f8ac80b632a9dd265d7f04066588
Julia Astakhova <jastakhova@griddynamics.com>
2015-09-27 13:29:48 -0700

    home and teacher page

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # piano-lessons-site
 Internet Site for Piano Lessons Teacher
 
-to test locally run "python -m SimpleHTTPServer"
+To test locally run "python -m SimpleHTTPServer" and open browser at http://localhost:8000/
+
+To enable animation add "ngAnimate" to app.js, include angular-animate.js in index.html and add this CSS:
+ 
+ .carousel .item {
+    transition: opacity 0.5s;
+ }
+ .carousel .active.left, .carousel .active.right {
+     left:0;
+     opacity:0;
+     z-index:2;
+ }
+ .carousel .next {
+     left:0;
+     opacity:1;
+     z-index:1;
+ }
--- js/app.js
+++ js/app.js
@@ -1,24 +1,24 @@
 (function() {
-  var app = angular.module('studio', ['ui.bootstrap']);
+  var app = angular.module('studio', ['ui.bootstrap'/*, 'ngAnimate'*/]);
   
   app.controller('IndexController', function() {
     this.sections = ["Home", "Teacher", "Lessons", "Students", "Calendar", "Contact"];
-    
+
     this.activesection = "home";
-    
+
     this.activateSection = function(section) {
       this.activesection = section;
     };
-    
+
     this.isActive = function(section) {
       return this.activesection === section;
     };
-    
-    this.carouselInterval = 2000;
+
+    this.carouselInterval = 5000;
     
     this.slides = [];
     
-    for (var i=0; i<2; i++) {
+    for (var i = 0; i < 2; i++) {
       this.slides.push({image:"img/carousel" + (i + 1) + ".jpeg"});
     }
   });
